692aab8ffec8729104533ab20c0d2103549776ab
alxsshv <alxsshv@gmail.com>
2022-05-30 18:49:11 +0300

    version 2

--- src/main.java
+++ src/main.java
@@ -1,35 +1,68 @@
 public class main {
     public static void main(String[] args) {
-        int vasyaAge = 29;
-        int katyaAge = 29;
-        int mishaAge = 36;
+        int vasyaAge = 3;
+        int katyaAge = 17;
+        int mishaAge = 17;
 
         int min = -1;
         int middle = -1;
         int max = -1;
 
-        if (mishaAge > katyaAge && mishaAge > vasyaAge || mishaAge == katyaAge && mishaAge > vasyaAge) {
-            max = mishaAge;
-        } else if (katyaAge > mishaAge && katyaAge > vasyaAge || katyaAge == vasyaAge && katyaAge > mishaAge) {
-            max = katyaAge;
-        } else {
-            max = vasyaAge;
-        }
-
-        if (katyaAge < mishaAge && katyaAge < vasyaAge || katyaAge == mishaAge && katyaAge < vasyaAge){
-            min = katyaAge;
-        } else if (vasyaAge < katyaAge && vasyaAge < mishaAge || vasyaAge == mishaAge && vasyaAge < katyaAge){
-            min = vasyaAge;
-        } else {
+        if (mishaAge==vasyaAge && mishaAge == katyaAge) {
             min = mishaAge;
-        }
-
-        if (min == katyaAge && max == mishaAge || min == mishaAge && max == katyaAge){
             middle = vasyaAge;
-        } else if (min == vasyaAge && max == mishaAge || min == mishaAge && max == vasyaAge){
-            middle = katyaAge;
+            max = katyaAge;
+        }
+        if (mishaAge < katyaAge || mishaAge < vasyaAge) {
+            if (katyaAge < vasyaAge) {
+                max = vasyaAge;
+                if (mishaAge < katyaAge || mishaAge == katyaAge) {
+                 min = mishaAge;
+                 middle = katyaAge;
+                } else {
+                 min = katyaAge;
+                 middle = mishaAge;
+                }
+            } else {
+                max = katyaAge;
+                if (mishaAge < vasyaAge || mishaAge == vasyaAge){
+                    min = mishaAge;
+                    middle = vasyaAge;
+                } else {
+                    min = vasyaAge;
+                    middle = mishaAge;
+                }
+            }
+        }
+        if (katyaAge < mishaAge || katyaAge < vasyaAge) {
+            if (vasyaAge < mishaAge) {
+                max = mishaAge;
+                if (katyaAge < vasyaAge || katyaAge == vasyaAge){
+                    min = katyaAge;
+                    middle = vasyaAge;
+                } else {
+                    min = vasyaAge;
+                    middle = katyaAge;
+                }
+            } else {
+                max = vasyaAge;
+                if (katyaAge < mishaAge || katyaAge == mishaAge) {
+                    middle = mishaAge;
+                    min = katyaAge;
+                } else {
+                    middle = katyaAge;
+                    min = mishaAge;
+                }
+            }
         } else {
-            middle = mishaAge;
+            max = katyaAge;
+            if (mishaAge < vasyaAge || mishaAge == vasyaAge) {
+                min = mishaAge;
+                middle = vasyaAge;
+            } else {
+                min = vasyaAge;
+                middle = mishaAge;
+            }
         }
 
         System.out.println("Minimal age: "+ min);
